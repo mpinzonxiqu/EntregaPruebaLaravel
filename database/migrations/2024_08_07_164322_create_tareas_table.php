86fde4af84b86ce27645dc8ec69cceed68ecfe42
mpinzonxiqu <31393338+mpinzonxiqu@users.noreply.github.com>
2024-08-07 12:43:50 -0500

    Add files via upload

---
+++ database/migrations/2024_08_07_164322_create_tareas_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tareas', function (Blueprint $table) {
+            $table->id();
+            $table->string('usuario_id');
+            $table->string('titulo');
+            $table->string('descripcion');
+            $table->string('fecha_vencimiento');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tareas');
+    }
+};
